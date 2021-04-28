63672deb41310cce1b32394ea6572ea95c3a21bb
Emeraldong <83355719+Emeraldong@users.noreply.github.com>
2021-04-28 22:23:28 +0800

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+<title>Hello world page</title>
+</head>
+ <body>
+ Hello HTML!
+<?php   // declare string variable $output
+ $output = "<b>Hello PHP!</b>";  Echo $output; ?>
+<?php $helloworldvariable= "<h1>AAAAAND HEEEEEELLOOOOOOO WOOOOOOOORRRRRLD!</h1>"; Echo $helloworldvariable; ?>
+ </body>
+ </html> 
